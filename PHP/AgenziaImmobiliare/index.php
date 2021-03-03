d2cfee534cdfeccbd6727da052a215d01cfd33bd
Federico <federico.bertoncini@gmail.com>
2021-03-03 19:28:31 +0100

    update

--- PHP/AgenziaImmobiliare/index.php
+++ PHP/AgenziaImmobiliare/index.php
@@ -1,3 +1,40 @@
+<?php
+
+session_start();
+
+if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] != true) {
+    header("location: signin.php");
+}
+
+require_once "config.php";
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -97,31 +134,31 @@
           </button>
           <!-- LOGO -->
           <!-- Text based logo -->
-          <a class="navbar-brand aa-logo" href="index.html"> Home <span>Property</span></a>
+          <a class="navbar-brand aa-logo" href="index.php"> Home <span>Proprietà</span></a>
           <!-- Image based logo -->
-          <!-- <a class="navbar-brand aa-logo-img" href="index.html"><img src="img/logo.png" alt="logo"></a> -->
+          <!-- <a class="navbar-brand aa-logo-img" href="index.php"><img src="img/logo.png" alt="logo"></a> -->
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul id="top-menu" class="nav navbar-nav navbar-right aa-main-nav">
-            <li class="active"><a href="index.html">HOME</a></li>
+            <li class="active"><a href="index.php">HOME</a></li>
             <li class="dropdown">
-              <a class="dropdown-toggle" data-toggle="dropdown" href="properties.html">PROPERTIES <span
+              <a class="dropdown-toggle" data-toggle="dropdown" href="properties.html">PROPRIETÀ <span
                   class="caret"></span></a>
               <ul class="dropdown-menu" role="menu">
-                <li><a href="properties.html">PROPERTIES</a></li>
-                <li><a href="properties-detail.html">PROPERTIES DETAIL</a></li>
+                <li><a href="properties.html">Proprietà</a></li>
+                <li><a href="properties-detail.html">Dettagli proprietà</a></li>
               </ul>
             </li>
-            <li><a href="gallery.html">GALLERY</a></li>
+            <li><a href="gallery.html">GALLERIA</a></li>
             <li class="dropdown">
               <a class="dropdown-toggle" data-toggle="dropdown" href="blog-archive.html">BLOG <span
                   class="caret"></span></a>
               <ul class="dropdown-menu" role="menu">
-                <li><a href="blog-archive.html">BLOG</a></li>
-                <li><a href="blog-single.html">BLOG DETAILS</a></li>
+                <li><a href="blog-archive.html">Blog</a></li>
+                <li><a href="blog-single.html">Dettagli Blog</a></li>
               </ul>
             </li>
-            <li><a href="contact.html">CONTACT</a></li>
+            <li><a href="contact.html">CONTATTACI</a></li>
             <li><a href="404.html">404 PAGE</a></li>
           </ul>
         </div>
@@ -138,15 +175,14 @@
       <div class="aa-top-slider">
         <!-- Top slider single slide -->
         <div class="aa-top-slider-single">
-          <img src="img/slider/3.jpg" alt="img">
+          <img src="img/slider/1.jpg" alt="img">
           <!-- Top slider content -->
           <div class="aa-top-slider-content">
-            <span class="aa-top-slider-catg">Duplex</span>
-            <h2 class="aa-top-slider-title">1560 Square Feet</h2>
-            <p class="aa-top-slider-location"><i class="fa fa-map-marker"></i>South Beach, Miami (USA)</p>
-            <span class="aa-top-slider-off">30% OFF</span>
-            <p class="aa-top-slider-price">$460,000</p>
-            <a href="#" class="aa-top-slider-btn">Read More <span class="fa fa-angle-double-right"></span></a>
+            <span class="aa-top-slider-catg">Villa a Gavorrano</span>
+            <h4 class="aa-top-slider-title">450 mq</h4>
+            <p class="aa-top-slider-location"><i class="fa fa-map-marker"></i>Castellaccia, Grosseto (TOS)</p>
+            <p class="aa-top-slider-price">€2.300.000</p>
+            <a href="#" class="aa-top-slider-btn">Informazioni <span class="fa fa-angle-double-right"></span></a>
           </div>
           <!-- / Top slider content -->
         </div>
@@ -156,42 +192,25 @@
           <img src="img/slider/2.jpg" alt="img">
           <!-- Top slider content -->
           <div class="aa-top-slider-content">
-            <span class="aa-top-slider-catg">Duplex</span>
-            <h2 class="aa-top-slider-title">1560 Square Feet</h2>
-            <p class="aa-top-slider-location"><i class="fa fa-map-marker"></i>South Beach, Miami (USA)</p>
-            <span class="aa-top-slider-off">30% OFF</span>
-            <p class="aa-top-slider-price">$460,000</p>
-            <a href="#" class="aa-top-slider-btn">Read More <span class="fa fa-angle-double-right"></span></a>
+            <span class="aa-top-slider-catg">Villa a Merate</span>
+            <h4 class="aa-top-slider-title">800 mq</h4>
+            <p class="aa-top-slider-location"><i class="fa fa-map-marker"></i>Merate, Como (LOM)</p>
+            <p class="aa-top-slider-price">Prezzo su richiesta</p>
+            <a href="#" class="aa-top-slider-btn">Informazioni <span class="fa fa-angle-double-right"></span></a>
           </div>
           <!-- / Top slider content -->
         </div>
         <!-- / Top slider single slide -->
         <!-- Top slider single slide -->
         <div class="aa-top-slider-single">
-          <img src="img/slider/1.jpg" alt="img">
+          <img src="img/slider/3.jpg" alt="img">
           <!-- Top slider content -->
           <div class="aa-top-slider-content">
-            <span class="aa-top-slider-catg">Duplex</span>
-            <h2 class="aa-top-slider-title">1560 Square Feet</h2>
-            <p class="aa-top-slider-location"><i class="fa fa-map-marker"></i>South Beach, Miami (USA)</p>
-            <span class="aa-top-slider-off">30% OFF</span>
-            <p class="aa-top-slider-price">$460,000</p>
-            <a href="#" class="aa-top-slider-btn">Read More <span class="fa fa-angle-double-right"></span></a>
-          </div>
-          <!-- / Top slider content -->
-        </div>
-        <!-- / Top slider single slide -->
-        <!-- Top slider single slide -->
-        <div class="aa-top-slider-single">
-          <img src="img/slider/5.jpg" alt="img">
-          <!-- Top slider content -->
-          <div class="aa-top-slider-content">
-            <span class="aa-top-slider-catg">Duplex</span>
-            <h2 class="aa-top-slider-title">1560 Square Feet</h2>
-            <p class="aa-top-slider-location"><i class="fa fa-map-marker"></i>South Beach, Miami (USA)</p>
-            <span class="aa-top-slider-off">30% OFF</span>
-            <p class="aa-top-slider-price">$460,000</p>
-            <a href="#" class="aa-top-slider-btn">Read More <span class="fa fa-angle-double-right"></span></a>
+            <span class="aa-top-slider-catg">Villa Antinori di Monte Aguglioni</span>
+            <h4 class="aa-top-slider-title">4000 mq</h4>
+            <p class="aa-top-slider-location"><i class="fa fa-map-marker"></i>Scandicci, Firenze (TOS)</p>
+            <p class="aa-top-slider-price">€30.000.000</p>
+            <a href="#" class="aa-top-slider-btn">Informazioni <span class="fa fa-angle-double-right"></span></a>
           </div>
           <!-- / Top slider content -->
         </div>
@@ -201,27 +220,25 @@
           <img src="img/slider/4.jpg" alt="img">
           <!-- Top slider content -->
           <div class="aa-top-slider-content">
-            <span class="aa-top-slider-catg">Duplex</span>
-            <h2 class="aa-top-slider-title">1560 Square Feet</h2>
-            <p class="aa-top-slider-location"><i class="fa fa-map-marker"></i>South Beach, Miami (USA)</p>
-            <span class="aa-top-slider-off">30% OFF</span>
-            <p class="aa-top-slider-price">$460,000</p>
-            <a href="#" class="aa-top-slider-btn">Read More <span class="fa fa-angle-double-right"></span></a>
+            <span class="aa-top-slider-catg">Villa di inizio '900</span>
+            <h4 class="aa-top-slider-title">700 mq</h4>
+            <p class="aa-top-slider-location"><i class="fa fa-map-marker"></i>Firenze, Firenze, (TOS)</p>
+            <p class="aa-top-slider-price">€7.450.000</p>
+            <a href="#" class="aa-top-slider-btn">Informazioni <span class="fa fa-angle-double-right"></span></a>
           </div>
           <!-- / Top slider content -->
         </div>
         <!-- / Top slider single slide -->
         <!-- Top slider single slide -->
         <div class="aa-top-slider-single">
-          <img src="img/slider/6.jpg" alt="img">
+          <img src="img/slider/5.jpg" alt="img">
           <!-- Top slider content -->
           <div class="aa-top-slider-content">
-            <span class="aa-top-slider-catg">Duplex</span>
-            <h2 class="aa-top-slider-title">1560 Square Feet</h2>
-            <p class="aa-top-slider-location"><i class="fa fa-map-marker"></i>South Beach, Miami (USA)</p>
-            <span class="aa-top-slider-off">30% OFF</span>
-            <p class="aa-top-slider-price">$460,000</p>
-            <a href="#" class="aa-top-slider-btn">Read More <span class="fa fa-angle-double-right"></span></a>
+            <span class="aa-top-slider-catg">Castello in Chianti</span>
+            <h4 class="aa-top-slider-title">5000 mq + 100 ettari</h4>
+            <p class="aa-top-slider-location"><i class="fa fa-map-marker"></i>Chianti, Firenze (TOS)</p>
+            <p class="aa-top-slider-price">TRATTATIVA RISERVATA</p>
+            <a href="#" class="aa-top-slider-btn">Informazioni <span class="fa fa-angle-double-right"></span></a>
           </div>
           <!-- / Top slider content -->
         </div>
@@ -240,17 +257,17 @@
             <div class="row">
               <div class="col-md-4">
                 <div class="aa-single-advance-search">
-                  <input type="text" placeholder="Type Your Location">
+                  <input type="text" placeholder="Città/CAP">
                 </div>
               </div>
               <div class="col-md-2">
                 <div class="aa-single-advance-search">
                   <select>
-                    <option value="0" selected>Category</option>
-                    <option value="1">Flat</option>
-                    <option value="2">Land</option>
-                    <option value="3">Plot</option>
-                    <option value="4">Commercial</option>
+                    <option value="0" selected>Categoria</option>
+                    <option value="1">Appartamento</option>
+                    <option value="2">Casa</option>
+                    <option value="3">Garage</option>
+                    <option value="4">Lusso</option>
                   </select>
                 </div>
               </div>
@@ -287,10 +304,10 @@
             <div class="row">
               <div class="col-md-6">
                 <div class="aa-single-filter-search">
-                  <span>AREA (SQ)</span>
-                  <span>FROM</span>
+                  <span>SUPERFICIE (MQ)</span>
+                  <span>DA</span>
                   <span id="skip-value-lower" class="example-val">30.00</span>
-                  <span>TO</span>
+                  <span>A</span>
                   <span id="skip-value-upper" class="example-val">100.00</span>
                   <div id="aa-sqrfeet-range" class="noUi-target noUi-ltr noUi-horizontal noUi-background">
                   </div>
@@ -298,10 +315,10 @@
               </div>
               <div class="col-md-6">
                 <div class="aa-single-filter-search">
-                  <span>PRICE ($)</span>
-                  <span>FROM</span>
+                  <span>PREZZO (€)</span>
+                  <span>DA</span>
                   <span id="skip-value-lower2" class="example-val">30.00</span>
-                  <span>TO</span>
+                  <span>A</span>
                   <span id="skip-value-upper2" class="example-val">100.00</span>
                   <div id="aa-price-range" class="noUi-target noUi-ltr noUi-horizontal noUi-background">
                   </div>
